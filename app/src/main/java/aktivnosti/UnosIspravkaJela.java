da1e2cc6a131ffb57cb5a39bf1b1064d3865ed72
borcha <borisbokan@gmail.com>
2017-06-02 23:21:59 +0200

    Napravljena nova aktivnost za unos i ispravku Jela..sutra nastavak

---
+++ app/src/main/java/aktivnosti/UnosIspravkaJela.java
@@ -0,0 +1,24 @@
+package aktivnosti;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import rs.aleph.android.jelovnik.R;
+
+/**
+ * Created by borcha on 02.06.17..
+ */
+
+public class UnosIspravkaJela extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addContentView(R.layout.unos_ispravka_jela);
+
+
+
+
+    }
+}
